71af0df28a26bc5e5a359f883ab6395916e71b04
Gavin Hung <gavinkhung@gmail.com>
2020-11-18 04:02:50 -0800

    added challenge 2 for day 3

---
+++ day3_challenge2/src/main/java/frc/robot/OI.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.DriveCommand;
+
+/**
+ * Add your docs here.
+ */
+public class OI {
+
+    private Joystick joystick;
+    private JoystickButton startButton, endButton;
+
+    public OI(){
+        joystick = new Joystick(0);
+        startButton = new JoystickButton(joystick, 1);
+
+        startButton.whenPressed(new DriveCommand());
+    }
+
+    public boolean endButtonIsPressed(){
+        return endButton.get();
+    }
+
+    public double getX(){
+        return joystick.getRawAxis(0);
+    }
+}
